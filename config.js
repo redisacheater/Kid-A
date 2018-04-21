4bd83b54b2c649519d29fe761be761da88674ecb
redisacheater <38130913+redisacheater@users.noreply.github.com>
2018-04-21 11:41:38 +0530

    Update and rename config-example.js to config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ exports.host = 'sim.smogon.com';
 exports.port = 8000;
 
 // Host and port to use for the http server part of Kid A.
-exports.serverhost = 'localhost';
-exports.serverport = 8000;
+exports.serverhost = 'http://orderofphoenix.herokuapp.com-80.psim.us/';
+exports.serverport = 80;
 
 // Enter a value (or load a file) for these if you want to use https and SSL.
 exports.sslCert = '';
@@ -11,8 +11,8 @@ exports.sslKey = '';
 exports.sslCa = '';
 
 // Username and password to use on PS.
-exports.username = '';
-exports.password = '';
+exports.username = 'rohan99';
+exports.password = 'faps9810';
 
 // Rooms to join and avatar to choose. The maximum amount of rooms Kid A can join upon connecting is 11.
 // The reason for these restrictions is the way PS protocol works. I might try to get around it at a later date, but this is it for now.
